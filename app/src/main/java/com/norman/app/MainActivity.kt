4a951747bc7d4ec3a85bc0016a59f4bd319072b7
maxiaobu1999 <maqinglong1999@163.com>
2019-07-17 15:23:11 +0800

    Initial commit

---
+++ app/src/main/java/com/norman/app/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.norman.app
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener { view->
+            val intent= Intent(this@MainActivity,HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
